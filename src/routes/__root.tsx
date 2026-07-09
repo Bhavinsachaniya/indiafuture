7c931e7c96d09933d2dad77664916444fac979b5
Bhavin Sachaniya <bhavinbhavin494@gmail.com>
2026-07-09 20:39:06 +0530

    Add TanStack Router ScrollRestoration for proper navigation scrolling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ import {
   useRouter,
   HeadContent,
   Scripts,
+  ScrollRestoration,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
@@ -106,6 +107,7 @@ function RootShell({ children }: { children: ReactNode }) {
       </head>
       <body>
         {children}
+        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
