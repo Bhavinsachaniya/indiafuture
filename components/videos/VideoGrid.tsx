7d4cff027694e77cef6d59cd66e633e32aa0fe3a
Bhavin Sachaniya <bhavinbhavin494@gmail.com>
2026-07-13 11:58:20 +0530

    style: change student creations grid layout to 3 columns

--- components/videos/VideoGrid.tsx
+++ components/videos/VideoGrid.tsx
@@ -36,7 +36,7 @@ export function VideoGrid({ videos, onVideoClick }: VideoGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {videos.map((video, index) => (
         <VideoCard key={video.id} video={video} index={index} onClick={onVideoClick} />
       ))}
