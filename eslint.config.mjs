8ca1a1fdc52ac35accab615d88f06e1d759caa3c
Bhavin Sachaniya <151622027+Bhavinsachaniya@users.noreply.github.com>
2026-07-16 12:41:26 +0530

    Rename eslint.config.js to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: [".next", "node_modules"] },
+  { ignores: [".next", "node_modules", "out", "dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
@@ -15,10 +16,15 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
-  eslintPluginPrettier,
   {
-    rules: {
-      "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@next/next": nextPlugin,
     },
-  }
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  eslintPluginPrettier,
 );
